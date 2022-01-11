3fa0ee9619076c22ecef5b4f07fc443faa428cc8
choi-sus <choi-sus@naver.com>
2022-01-11 13:52:13 +0900

    주석

--- app.py
+++ app.py
@@ -3,16 +3,18 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
+# 페이지 이동
+## 메인페이지
 @app.route('/')
 def main():
     return render_template("main.html")
 
-
+##상세페이지
 @app.route('/sub')
 def sub():
     return render_template("sub.html")
 
+##로그인페이지
 @app.route('/register')
 def register():
     return render_template('register.html')

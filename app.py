2fa2ae4619b4eceb79c0e63d72980e85cbd5fc27
choi-sus <choi-sus@naver.com>
2022-01-11 16:15:42 +0900

    페이지 이동

--- app.py
+++ app.py
@@ -15,9 +15,9 @@ def sub():
     return render_template("sub.html")
 
 ##로그인페이지
-@app.route('/register')
+@app.route('/login')
 def register():
-    return render_template('register.html')
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
